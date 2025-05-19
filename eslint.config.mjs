e17824d2f32dd0e9560a8fc38ada7d8c00a7aba7
Max Beyer <max.beyer09@gmail.com>
2025-05-19 00:07:45 -0500

    feat: add GitHub link to landing page and error page with hover effect

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,16 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], rules: { "no-unused-vars": "warn" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: { 'no-unused-vars': 'warn', 'no-useless-escape': 'warn' },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.browser },
+  },
 ]);
